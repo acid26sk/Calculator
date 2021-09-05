cb2617ba246c525ad4570a67852417007e9c3c9c
Programmer <acid26sk@gmail.com>
2021-09-05 20:29:55 +0300

    Перехожу к кнопкам выбора меню

--- app/src/main/java/com/example/calculator/Sitings.java
+++ app/src/main/java/com/example/calculator/Sitings.java
@@ -4,32 +4,26 @@ package com.example.calculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Sitings extends AppCompatActivity {
 
-    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
 
-        btnBack = (Button) findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(this::onClick);
+        findViewById(R.id.btnBack).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Sitings.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnBack:
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                break;
-            default:
-                break;
-        }
-    }
+
 
 }
